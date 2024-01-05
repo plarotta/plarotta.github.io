172b2511d7966ce021658ace54a466c4b2b2c479
Pedro Leandro La Rotta <plarottaone2@gmail.com>
2024-01-05 15:22:15 -0500

    Update setup.js

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -1,114 +1,121 @@
 // Start up particles in background
 particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 125,
-      "density": {
-        "enable": true,
-        "value_area": 800
+    particles: {
+    number: {
+      value: 400,
+      density: {
+        enable: true,
+        value_area: 800
       }
     },
-    "color": {
-      "value": "#ababab"
+    color: {
+      value: '#fff'
     },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#ff0000'
       },
-      "polygon": {
-        "nb_sides": 9
+      polygon: {
+        nb_sides: 5
       },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
+      image: {
+        src: '',
+        width: 100,
+        height: 100
       }
     },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 5.409158431828455,
-        "opacity_min": 0.1,
-        "sync": false
+    opacity: {
+      value: 1,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 2,
+        opacity_min: 0,
+        sync: false
       }
     },
-    "size": {
-      "value": 2,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 174.3848091455144,
-        "size_min": 9.688044952528577,
-        "sync": false
+    size: {
+      value: 20,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 20,
+        size_min: 0,
+        sync: false
       }
     },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ababab",
-      "opacity": 0.4,
-      "width": 1
+    line_linked: {
+      enable: true,
+      distance: 100,
+      color: '#fff',
+      opacity: 1,
+      width: 1
     },
-    "move": {
-      "enable": true,
-      "speed": 2,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "bounce",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
+    move: {
+      enable: true,
+      speed: 2,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 3000,
+        rotateY: 3000
       }
-    }
+    },
+    array: []
   },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "grab"
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'grab'
       },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
+      onclick: {
+        enable: true,
+        mode: 'push'
       },
-      "resize": true
+      resize: true
     },
-    "modes": {
-      "grab": {
-        "distance": 205.87095524123225,
-        "line_linked": {
-          "opacity": 1
+    modes: {
+      grab:{
+        distance: 100,
+        line_linked:{
+          opacity: 1
         }
       },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
+      bubble:{
+        distance: 200,
+        size: 80,
+        duration: 0.4
       },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
+      repulse:{
+        distance: 200,
+        duration: 0.4
       },
-      "push": {
-        "particles_nb": 4
+      push:{
+        particles_nb: 4
       },
-      "remove": {
-        "particles_nb": 2
+      remove:{
+        particles_nb: 2
       }
-    }
+    },
+    mouse:{}
   },
-  "retina_detect": true
-});
+  retina_detect: false,
+  fn: {
+    interact: {},
+    modes: {},
+    vendors:{}
+  },
+  tmp: {}
+};
+
 
 // HELPER FUNCTIONS
 function setColor(c, smooth_transition = true) {
@@ -189,55 +196,6 @@ function collapseAndAddFade(listItemId) {
   }
 }
 
-// Check if a cookie with color settings exist
-var bg_color = document.cookie.replace(
-    /(?:(?:^|.*;\s*)background-color\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-if (bg_color) {
-  // If so, then use those settings to set class=selected to selected color
-  $('.color-option[data-palette="'+bg_color+'"]').addClass('selected');
-} else {
-  // ...else default to 'sepia'...
-  var DEFAULT = "dark";
-  $('.color-option[data-palette="'+DEFAULT+'"]').addClass('selected');
-  // ...and init cookie
-  setCookie(DEFAULT);
-}
-
-// Set CSS depending on selected color
-var color = $(".color-option.selected").attr('data-palette');
-setColor(color, /*smooth_transition=*/false);
-
-// Set click handlers for color palette
-$(document).on('click', '.color-option[data-palette="light"]',
-  function() {
-    $(".color-option.selected").removeClass('selected');
-    $(this).addClass('selected');
-
-    setColor("light");
-    setCookie("light");
-  }
-);
-$(document).on('click', '.color-option[data-palette="dark"]',
-  function() {
-    $(".color-option.selected").removeClass('selected');
-    $(this).addClass('selected');
-
-    setColor("dark");
-    setCookie("dark");
-  }
-);
-$(document).on('click', '.color-option[data-palette="sepia"]',
-  function() {
-    $(".color-option.selected").removeClass('selected');
-    $(this).addClass('selected');
-
-    setColor("sepia");
-    setCookie("sepia");
-  }
-);
-
-// Email obfuscation
-// see: https://css-tricks.com/how-to-safely-share-your-email-address-on-a-website/
 var encodedEmail = "aXp6eUBpenp5Z29tZXouY29t" // should be {{ site.email }}
 const emailElement = document.getElementById("email")
 emailElement.setAttribute("href", "mailto:".concat(atob(encodedEmail)))
